a0866f8713a0ce68e2548ce1dd28d1bf4aee6f18
markus <m.keuter@quintgroup.com>
2017-01-13 11:16:39 +0100

    ionicCreator files
    downloaded from playing on ionic creator

---
+++ js/routes.js
@@ -0,0 +1,70 @@
+angular.module('app.routes', ['ionicUIRouter'])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('tabsController.cameraTabDefaultPage', {
+    url: '/page2',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/cameraTabDefaultPage.html',
+        controller: 'cameraTabDefaultPageCtrl'
+      }
+    }
+  })
+
+  /* 
+    The IonicUIRouter.js UI-Router Modification is being used for this route.
+    To navigate to this route, do NOT use a URL. Instead use one of the following:
+      1) Using the ui-sref HTML attribute:
+        ui-sref='tabsController.cartTabDefaultPage'
+      2) Using $state.go programatically:
+        $state.go('tabsController.cartTabDefaultPage');
+    This allows your app to figure out which Tab to open this page in on the fly.
+    If you're setting a Tabs default page or modifying the .otherwise for your app and
+    must use a URL, use one of the following:
+      /page1/tab1/page3
+      /page1/tab2/page3
+  */
+  .state('tabsController.cartTabDefaultPage', {
+    url: '/page3',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/cartTabDefaultPage.html',
+        controller: 'cartTabDefaultPageCtrl'
+      },
+      'tab2': {
+        templateUrl: 'templates/cartTabDefaultPage.html',
+        controller: 'cartTabDefaultPageCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.cloudTabDefaultPage', {
+    url: '/page4',
+    views: {
+      'tab3': {
+        templateUrl: 'templates/cloudTabDefaultPage.html',
+        controller: 'cloudTabDefaultPageCtrl'
+      }
+    }
+  })
+
+  .state('tabsController', {
+    url: '/page1',
+    templateUrl: 'templates/tabsController.html',
+    abstract:true
+  })
+
+$urlRouterProvider.otherwise('/page1/page4')
+
+  
+
+});
